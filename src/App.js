ced659c80533247c53be84a3e7eaa13d9ec74d45
DevMasen <mohammadwh400@gmail.com>
2026-02-09 21:40:35 +0330

    feat: loading data from fake API implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
+const initialState = {
+	questions: [],
+
+	// status: loading, error, ready, active, finished
+	status: 'loading',
+};
+function reducer(state, action) {
+	switch (action.type) {
+		case 'dataReceived':
+			return {
+				...state,
+				questions: action.payload,
+				status: 'ready',
+			};
+		case 'dataFailed':
+			return {
+				...state,
+				status: 'error',
+			};
+		default:
+			return new Error('Unknown Action!');
+	}
+}
+
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	console.log(state);
+	useEffect(function () {
+		async function getQuestions() {
+			try {
+				const res = await fetch('http://localhost:8000/questions');
+				if (!res.ok) {
+					throw new Error('Error!');
+				}
+				const data = await res.json();
+				dispatch({ type: 'dataReceived', payload: data });
+			} catch (err) {
+				dispatch({ type: 'dataFailed' });
+			}
+		}
+		getQuestions();
+	}, []);
 	return (
 		<div className="app">
 			<Header />
